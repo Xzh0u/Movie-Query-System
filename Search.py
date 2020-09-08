3cae8bc340dafe9e9e41911653d4ef4aa8ce1c2d
Xzh0u <littleland.sxz@gmail.com>
2020-09-08 18:33:07 +0800

    feat: create the movieitems class and start functions

--- Search.py
+++ Search.py
@@ -1,8 +1,26 @@
 import csv
 
-with open('Movies250.csv') as f:
-    a = [{k: v
-          for k, v in row.items()}
-         for row in csv.DictReader(f, skipinitialspace=True)]
 
-print(a)
+class MovieItems(object):
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    # taged movie items
+    def getTagedItems(self, tag: dict):
+        pass
+
+    # sorted movie items
+    def getSortedItems():
+        pass
+
+    # all movie items
+    def getAllItems(filename: str):
+        with open('Movies250.csv') as f:
+            a = [{k: v
+                  for k, v in row.items()}
+                 for row in csv.DictReader(f, skipinitialspace=True)]
+        return a
+
+
+if __name__ == "__main__":
+    print(allMovieItems())
