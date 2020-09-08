387d26050b0246b69517e46846416c946369854f
Xzh0u <littleland.sxz@gmail.com>
2020-09-08 18:55:02 +0800

    docs: add documents for the class and functions

--- Search.py
+++ Search.py
@@ -1,25 +1,55 @@
 import csv
 
 
+# MovieItems:
+# 根据输入条件进行计算，返回所需展示的电影条目
+#
+# 参数:
+#        filename -- 获取数据的文件路径
+#
+# 返回:
+#        展示的电影条目
 class MovieItems(object):
     def __init__(self, filename: str):
         self.filename = filename
 
-    # taged movie items
+    # getTagedItems:
+    # 返回所有特定分类的条目
+    #
+    # 参数:
+    #        tag -- 用户选取的分类
+    #
+    # 返回:
+    #        展示的电影条目
     def getTagedItems(self, tag: dict):
         pass
 
-    # sorted movie items
-    def getSortedItems():
+    # getSortedItems:
+    # 根据特定类别进行排序
+    #
+    # 参数:
+    #        tag -- 用户选取的分类
+    #
+    # 返回:
+    #        展示的电影条目
+    def getSortedItems(self, sortItem: str):
         pass
 
-    # all movie items
-    def getAllItems(filename: str):
+    # getAllItems：
+    # 返回所有电影条目
+    #
+    # 参数:
+    #        filename -- 获取数据的文件路径
+    #
+    # 返回:
+    #        所有电影条目
+    def getAllItems(self, filename: str):
         with open('Movies250.csv') as f:
-            a = [{k: v
-                  for k, v in row.items()}
-                 for row in csv.DictReader(f, skipinitialspace=True)]
-        return a
+            allItems = [{k: v
+                         for k, v in row.items()}
+                        for row in csv.DictReader(f, skipinitialspace=True)]
+        # TODO: shuffle
+        return allItems
 
 
 if __name__ == "__main__":
